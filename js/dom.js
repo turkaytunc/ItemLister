b7ee88c1feb7c0f0c3d316a8b050bb8837b4ab8a
Turkay Tunc <turkaytunc00@gmail.com>
2020-01-24 21:03:48 +0300

    Renklendirme butonu eklendi

--- js/dom.js
+++ js/dom.js
@@ -1,4 +1,5 @@
-function ChangeEveryOtherColor(arr, colorOdd, colorEven) {
+function ChangeEveryOtherColor(arr, colorOdd, colorEven, flag) {
+if (flag == 1) {
     for (let i = 0; i < arr.length; i++) {
         if (!(i % 2)) {
             arr[i].style.backgroundColor = `${colorEven}`;
@@ -7,8 +8,26 @@ function ChangeEveryOtherColor(arr, colorOdd, colorEven) {
             arr[i].style.backgroundColor = `${colorOdd}`;
         }
     }
+}
+else {
+    for (let i = 0; i < arr.length; i++) {
+        if (!(i % 2)) {
+            arr[i].style.backgroundColor = `${colorOdd}`;
+        }
+        else {
+            arr[i].style.backgroundColor = `${colorEven}`;
+        }
+    }
+}
 };
+
+document.getElementById("button").addEventListener('click', buttonClick);
+
 let doc = document.getElementsByClassName("list-group-item");
 let arr = [...doc];
 
-ChangeEveryOtherColor(arr, "#9DA9A0", "#A6D49F");
+let flag = 1;
+function buttonClick() {
+    ChangeEveryOtherColor(arr, "#9DA9A0", "#A6D49F", flag);
+    flag = !flag;
+}
