2c48f0f851775142e6a8fdee8d94d74de62b074d
Turkay Tunc <turkaytunc00@gmail.com>
2020-01-24 22:15:55 +0300

    item ekleme ve renk degistirme butonlari duzenlendi

--- js/dom.js
+++ js/dom.js
@@ -1,33 +1,46 @@
-function ChangeEveryOtherColor(arr, colorOdd, colorEven, flag) {
-if (flag == 1) {
+function changeEveryOtherColor(arr, colorOdd, colorEven, flag) {
+    if (flag == 1) {
+        this.color1 = colorEven;
+        this.color2 = colorOdd;  
+    }
+    else {
+        this.color1 = colorOdd;
+        this.color2 = colorEven;
+    }
     for (let i = 0; i < arr.length; i++) {
         if (!(i % 2)) {
-            arr[i].style.backgroundColor = `${colorEven}`;
+            arr[i].style.backgroundColor = `${color1}`;
         }
         else {
-            arr[i].style.backgroundColor = `${colorOdd}`;
+            arr[i].style.backgroundColor = `${color2}`;
         }
     }
-}
-else {
-    for (let i = 0; i < arr.length; i++) {
-        if (!(i % 2)) {
-            arr[i].style.backgroundColor = `${colorOdd}`;
-        }
-        else {
-            arr[i].style.backgroundColor = `${colorEven}`;
-        }
-    }
-}
 };
 
-document.getElementById("button").addEventListener('click', buttonClick);
-
-let doc = document.getElementsByClassName("list-group-item");
-let arr = [...doc];
+document.getElementById("button").addEventListener('click', changeColor);
+document.querySelector("#submit-button").addEventListener("click", addListItem);
 
 let flag = 1;
-function buttonClick() {
-    ChangeEveryOtherColor(arr, "#9DA9A0", "#A6D49F", flag);
+function changeColor(e) {
+    let doc = document.getElementsByClassName("list-group-item");
+    let arr = [...doc];
+    changeEveryOtherColor(arr, "#9DA9A0", "#A6D49F", flag);
+    if (flag == 1) {
+        document.getElementById("button").innerHTML = "Green Grey";
+    }
+    else
+        document.getElementById("button").innerHTML = "Grey Green";
     flag = !flag;
+}
+
+function addListItem() {
+    let newLi = document.createElement("li");
+    newLi.className = "list-group-item";
+    let inputValue = document.querySelector("#input-box").value;
+    if (inputValue) {
+        newLi.appendChild(document.createTextNode(`${inputValue}`));
+        document.querySelector(".list-group").appendChild(newLi); 
+        changeColor();
+    }
+    document.querySelector("#input-box").value = "";
 }
